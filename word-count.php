ce0b2850f774fac69436bd1a1c2ed50fb5a9a296
isrt09 <mazedur@gmail.com>
2021-01-18 01:42:49 +0600

    initial commit

---
+++ word-count.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Plugin Name: Word Count
+Plugin URI: https://word-count.com/
+Description: Word Counts for content writing marketing per articles in requested budgets counts
+Version: 1.0.1
+Author: Mazedur
+Author URI: https://mazedur.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: word-count
+*/
